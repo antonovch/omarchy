5e60c3329f41696b8e2499f253d45c4dfaf131ae
Anton Ovcharenko <antonovch@icloud.com>
2025-09-14 23:20:36 +0300

    comment out twitter webapp installation

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -6,10 +6,12 @@ omarchy-webapp-install "Grok" https://grok.com/manifest.webmanifest
 omarchy-webapp-install "Claude" https://claude.ai/manifest.json
 omarchy-webapp-install "YouTube" https://www.youtube.com/manifest.webmanifest https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 omarchy-webapp-install "GitHub" https://github.com/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
-omarchy-webapp-install "X" https://x.com/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png
 omarchy-webapp-install "Omarchy" https://learn.omacom.io/2/the-omarchy-manual https://pbs.twimg.com/profile_images/1960368050223685633/wOkAf1Zb_400x400.jpg
 omarchy-webapp-install "Hyprland" https://wiki.hypr.land/ https://wiki.hypr.land/favicon-dark.svg
+
+# broken manifest for x.com
+# omarchy-webapp-install "X" https://x.com/manifest.json
 
 # no manifest for Discord
 # omarchy-webapp-install "Discord" https://discord.com/channels/@me/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
