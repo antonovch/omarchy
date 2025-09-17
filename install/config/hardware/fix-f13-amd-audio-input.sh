462ca5e9818376cc0672a70ea730cdd5f8c63731
Anton Ovchareneko <antonovch@icloud.com>
2025-09-17 22:49:49 +0300

    add fix for dell/intel mic

--- install/config/hardware/fix-f13-amd-audio-input.sh
+++ install/config/hardware/fix-f13-amd-audio-input.sh
@@ -5,3 +5,10 @@ AMD_AUDIO_CARD=$(pactl list cards | grep -B20 "Family 17h/19h" | grep "Name: " |
 if [[ -n $AMD_AUDIO_CARD ]]; then
   pactl set-card-profile "$AMD_AUDIO_CARD" "HiFi (Mic1, Mic2, Speaker)" 2>/dev/null || true
 fi
+
+# Fix DELL laptop audio
+INTEL_AUDIO_CARD=$(pactl list cards | grep -B20 "alsa_card.pci-0000_00_1f.3" | grep "Name: " | awk '{print $2}')
+
+if [[ -n $INTEL_AUDIO_CARD ]]; then
+  pactl set-card-profile "$INTEL_AUDIO_CARD" output:analog-stereo+input:analog-stereo 2>/dev/null || true
+fi
