e7263d1b382119579b3f6df1adc51cc82cf55a4c
antonovch <antonovch@icloud.com>
2025-09-27 14:42:14 +0300

    use omarchy-pkg-add for zsh installations

--- install/packaging/zsh.sh
+++ install/packaging/zsh.sh
@@ -2,18 +2,12 @@
 
 echo "Installing zsh and oh-my-zsh..."
 
-# Install zsh
-yay -S --noconfirm zsh
+# Install packages
+omarchy-pkg-add zsh zsh-autosuggestions
 
 # Install oh-my-zsh non-interactively
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
-# Install zsh-autosuggestions plugin
-ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 fi
 
 # Set zsh as default shell
