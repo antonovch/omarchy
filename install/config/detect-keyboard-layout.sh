8d0ef96ea5bb8d7889ea310b042935dc9db80c9c
antonovch <antonovch@icloud.com>
2025-09-27 18:05:00 +0300

    do not add kb_layout in input.conf if already there

--- install/config/detect-keyboard-layout.sh
+++ install/config/detect-keyboard-layout.sh
@@ -4,7 +4,9 @@ hyprconf="$HOME/.config/hypr/input.conf"
 
 if grep -q '^XKBLAYOUT=' "$conf"; then
   layout=$(grep '^XKBLAYOUT=' "$conf" | cut -d= -f2 | tr -d '"')
-  sed -i "/^[[:space:]]*kb_options *=/i\  kb_layout = $layout" "$hyprconf"
+  if ! grep -q "kb_layout = $layout" "$hyprconf"; then
+    sed -i "/^[[:space:]]*kb_options *=/i\  kb_layout = $layout" "$hyprconf"
+  fi
 fi
 
 if grep -q '^XKBVARIANT=' "$conf"; then
