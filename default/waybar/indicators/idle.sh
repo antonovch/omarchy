895add7bfe4972dd67b1a039da379861e9229733
antonovch <antonovch@icloud.com>
2026-02-27 13:23:55 +0200

    update waybar idle indicator to omarchy style

--- default/waybar/indicators/idle.sh
+++ default/waybar/indicators/idle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if pgrep -x hypridle >/dev/null; then
-  echo '{"text": ""}'
+if pgrep -x hypridle >/dev/null ; then
+    echo '{"text": "", "class": "active", "tooltip": "Screen locking active\nLeft: Deactivate\nRight: Lock Screen"}'
 else
-  echo '{"text": "󱫖", "tooltip": "Idle lock disabled", "class": "active"}'
+    echo '{"text": "󱫖", "class": "notactive", "tooltip": "Screen locking deactivated\nLeft: Activate\nRight: Lock Screen"}'
 fi
