fe700582875d5879322d05768e5272a911c86c11
antonovch <antonovch@icloud.com>
2026-04-18 22:39:00 +0300

    google maps webapp

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
-omarchy-webapp-install "ChatGPT" https://chatgpt.com/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
+omarchy-webapp-install "WhatsApp" https://web.whatsapp.com/manifest.json WhatsApp.png
+omarchy-webapp-install "ChatGPT" https://chatgpt.com/manifest.json ChatGPT.png
 omarchy-webapp-install "Grok" https://grok.com/manifest.webmanifest
 omarchy-webapp-install "Claude" https://claude.ai/manifest.json
-omarchy-webapp-install "YouTube" https://www.youtube.com/manifest.webmanifest https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
-omarchy-webapp-install "GitHub" https://github.com/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+omarchy-webapp-install "YouTube" https://www.youtube.com/manifest.webmanifest YouTube.png
+omarchy-webapp-install "GitHub" https://github.com/manifest.json GitHub.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/manifest.json https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png
 omarchy-webapp-install "Omarchy" https://learn.omacom.io/2/the-omarchy-manual https://pbs.twimg.com/profile_images/1960368050223685633/wOkAf1Zb_400x400.jpg
 omarchy-webapp-install "Hyprland" https://wiki.hypr.land/ https://wiki.hypr.land/favicon-dark.svg
+omarchy-webapp-install "Google Maps" https://www.google.com/maps/preview/pwa/manifest "Google Maps.png"
 
 # broken manifest for x.com
-# omarchy-webapp-install "X" https://x.com/manifest.json
+# omarchy-webapp-install "X" https://x.com/manifest.json X.png
 
 # no manifest for Discord
 # omarchy-webapp-install "Discord" https://discord.com/channels/@me/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
